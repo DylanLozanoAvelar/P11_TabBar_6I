320f9f50fdbe2f2de4e8dcf9fb2a51eab13be927
Dylan_Lozano_Avelar <a.21308051280373@cbtis128.edu.mx>
2024-02-29 19:00:04 +0000

    Actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,89 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppMiTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppMiTabBar extends StatelessWidget {
+  const AppMiTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo TabBar Dylan Lozano",
       theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MiPaginaInicial(),
     );
-  }
-}
+  } // FIN Widget
+} // FIN AppMiTabBar
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+} // FIN MiPaginaInicial
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("TabBar Dylan Lozano"),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: "Gasolina",
+                icon: Icon(Icons.ev_station_rounded),
+              ),
+              Tab(
+                text: "Limpieza",
+                icon: Icon(Icons.cleaning_services_outlined),
+              ),
+              Tab(
+                text: "Sucursal",
+                icon: Icon(Icons.business),
+              ),
+              Tab(
+                text: "Extras",
+                icon: Icon(Icons.extension),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: const <Widget>[
+            Center(
+              child: Text(
+                "Magna",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Lavaparabrisas",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            ),
+            Center(
+              child: Text(
+                "PEMEX Chihuahua",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Llaveros",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            ),
+          ],
         ),
       ),
     );
-  }
-}
+  } // Widget
+} // _MiPaginaInicialState
